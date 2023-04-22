5a6320657048f0560edd1b2d95896e2808a038f9
sagarpokhrel <34935128+sagarpokhrel@users.noreply.github.com>
2023-04-23 01:25:52 +1200

    array

---
+++ arrays.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Arrays</title>
+    </head>
+    <body>
+        <?php
+        $name = array('Sagar','Nitish','Rabin');
+        echo $name[1]; //this is a single line comment for php
+        ?><br>
+
+        <!--  This section is html comment 
+        for the multiple lines-->
+
+        <?php 
+        ?>
+        
+
+
+    </body>
+
+</html>
+
+
--- constants.php
+++ constants.php
@@ -3,8 +3,12 @@ $NAME=45;
 define("value_of_Pi", 3.14);
 define("value_of_Gravity", 9.8);
 
-echo value_of_Pi;
-echo value_of_Gravity;
-echo "my name is sagar";
+echo "Pi Value =" . value_of_Pi ;
+echo "<br> G value =" . value_of_Gravity; //this is comment 
+echo "<br>";
+echo "$NAME <br>";
+echo strlen ("Sagar");
+echo  "<br>kings \n garden <br>";
+echo "Thanks for your email" ."<br>". "Your orders details are below:";
 
 ?>
